62c11ede812460be90b403c1474ec5f8ce810d23
maxwelltyreece <107719479+maxwelltyreece@users.noreply.github.com>
2023-09-15 19:02:21 +0100

    modals

--- src/components/projects/PortfolioSite.js
+++ src/components/projects/PortfolioSite.js
@@ -1,40 +1,62 @@
 import React from "react";
-import { Card, CardActionArea, CardActions, CardContent,CardMedia } from "@mui/material";
+import { Card, CardActionArea, CardActions, CardContent,CardMedia, Modal } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import PortfolioSite2 from "../../assets/PortfolioSite2.png";
+import { useState } from "react";
+import {faGithub} from '@fortawesome/free-brands-svg-icons'
+
 
 
 export default function PortfolioSite() {
-  return (
-    <Card sx={{ maxWidth: 350}}>
-        <CardActions className="bg-Space">
-            <a
-            href="https://github.com/maxwelltyreece"
-            rel="noopener noreferrer"
-            target="_blank"
-            
-            >
-            <FontAwesomeIcon icon={faUpRightFromSquare} size="xl"/>
-            </a>
 
-        </CardActions>
-        <CardActionArea>
-            <CardMedia
-                component="img"
-                height={10}
-                image={PortfolioSite2}
-                
-                alt="portfolio site"
-            />
-            <CardContent className="bg-Space ">
-                <p>My portfolio Site. You're currently browsing it!</p>
-            </CardContent>
-        </CardActionArea>
-        
-        
-    </Card>
-      
+    const[open,setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    
+    return (
+        <div>
+            <Card sx={{ maxWidth: 350}}>
+                <CardActionArea onClick={handleOpen}>
+                    <CardMedia
+                    component="img"
+                    height={10}
+                    image={PortfolioSite2}
+                    
+                    alt="portfolio site" />
+                    <CardContent className="bg-Space ">
+                        <p>My portfolio Site. You're currently browsing it!</p>
+                    </CardContent>               
+                </CardActionArea>
+            </Card>
+
+            <Modal open={open} onClose={handleClose} className="grid place-content-center">
+                <Card>
+                    <CardMedia/>
+                    <CardContent className="">
+                        <p>dskfghsdk ksdjhfsd kjshdrf skdjfh</p>
+                        <p>dskfghsdk ksdjhfsd kjshdrf skdjfh</p>
+                        <p>dskfghsdk ksdjhfsd kjshdrf skdjfh</p>
+                        <p>dskfghsdk ksdjhfsd kjshdrf skdjfh</p>
+
+
+                    </CardContent>
+                    <CardActions className="bg-Space justify-end">
+                        <a href="https://github.com/maxwelltyreece" rel="noopener noreferrer" target="_blank">
+                            <FontAwesomeIcon icon={faUpRightFromSquare} size="xl"/>
+                        </a>
+                        <a
+                            href="https://github.com/maxwelltyreece/Maxwellv2"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                            
+                            >
+                            <FontAwesomeIcon icon={faGithub} size="xl"/>
+                        </a>
+                    </CardActions>
+                </Card>
+            </Modal>
+        </div> 
     );
 
 }
--- src/components/projects/SchoolSimulator.js
+++ src/components/projects/SchoolSimulator.js
@@ -1,40 +1,64 @@
 import React from "react";
-import { Card, CardActionArea, CardActions, CardContent,CardMedia } from "@mui/material";
+import { Card, CardActionArea, CardActions, CardContent,CardMedia, Modal } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import {faGithub} from '@fortawesome/free-brands-svg-icons'
 import schoolImage from "../../assets/SchoolSimulator.png";
+import { useState } from "react";
 
 
 export default function SchoolSimulator() {
-  return (
-    <Card sx={{ maxWidth: 350}}>
-        <CardActions className="bg-Space">
-            <a
-            href="https://github.com/maxwelltyreece"
-            rel="noopener noreferrer"
-            target="_blank"
-            
-            >
-            <FontAwesomeIcon icon={faUpRightFromSquare} size="xl"/>
-            </a>
+    const[open,setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    
+    return (
+        <div>
+            <Card sx={{ maxWidth: 350}}>
+                <CardActionArea onClick={handleOpen}>
+                    <CardMedia
+                        component="img"
+                        height={10}
+                        image={schoolImage}
+                    
+                        alt="portfolio site"
+                    />
+                    <CardContent className="bg-Space ">
+                        <p>A game I developed inspired by Undertale and Zelda.</p>
+                    </CardContent>  
+                </CardActionArea>
+        
+        
+            </Card>
 
-        </CardActions>
-        <CardActionArea>
-            <CardMedia
-                component="img"
-                height={10}
-                image={schoolImage}
-                
-                alt="portfolio site"
-            />
-            <CardContent className="bg-Space ">
-                <p>A game I dewveloped inspired by Undertale and Zelda.</p>
-            </CardContent>
-        </CardActionArea>
-        
-        
-    </Card>
-      
+            <Modal open={open} onClose={handleClose} className="grid place-content-center">
+                <Card>
+                    <CardMedia/>
+                    <CardContent className="">
+                        <p>asfjkashf kjahsf kjsahfask kasjfhask kuashfas kjashf</p>
+                        <p>asfjkashf kjahsf kjsahfask kasjfhask kuashfas kjashf</p>
+                        <p>asfjkashf kjahsf kjsahfask kasjfhask kuashfas kjashf</p>
+                        <p>asfjkashf kjahsf kjsahfask kasjfhask kuashfas kjashf</p>
+                        <p>asfjkashf kjahsf kjsahfask kasjfhask kuashfas kjashf</p>
+                        <p>asfjkashf kjahsf kjsahfask kasjfhask kuashfas kjashf</p>
+
+                    </CardContent>
+                    <CardActions className="bg-Space justify-end">
+                        <a href="https://github.com/maxwelltyreece" rel="noopener noreferrer" target="_blank">
+                            <FontAwesomeIcon icon={faUpRightFromSquare} size="xl"/>
+                        </a>
+                        <a
+                            href="https://github.com/maxwelltyreece/School-Simulator"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                            
+                            >
+                            <FontAwesomeIcon icon={faGithub} size="xl"/>
+                        </a>
+                    </CardActions>
+                </Card>
+            </Modal>
+        </div> 
     );
 
 }
